72739bf89ee3407c556a9b3872c61e99d041f7ea
Julian COETZEE <jcoetzee@c5r2s4.wethinkcode.co.za>
2019-06-27 14:45:41 +0200

    Made files for GNL

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/26 14:52:50 by jcoetzee          #+#    #+#             */
+/*   Updated: 2019/06/26 15:00:54 by jcoetzee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.>
+# include <stdlib.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+int					get_next_line(const int fd, char **line);
+
+#endif
